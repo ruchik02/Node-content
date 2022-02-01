cb463400510682efa897420637c6f39d10f1c54c
ruchik02 <ruchika02>
2022-02-01 23:46:40 +0530

    create another controller and routes

---
+++ controllers/user_controller.js
@@ -0,0 +1,3 @@
+module.exports.profile=function(req,res){
+    res.end('<h1>Hello ,this is user profile page</h1>');
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require('express');
+const router = require('./routes');
 const app=express();
 const port=8000;
 
 // use express router
 app.use('/',require('./routes'));
-
+router.use('/users',require('./routes/users'));
 app.listen(port,function(err){
     if(err){
         console.log( `error: ${err}`);
