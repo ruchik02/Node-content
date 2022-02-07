bfc61bb1fad7d716add20c3ecec22ebaa62e060a
ruchik02 <ruchika02>
2022-02-07 12:52:25 +0530

    mvc structures

--- index.js
+++
@@ -1,9 +0,0 @@
-const fs=require('fs');
-// fs.writeFileSync('read.txt',"Welcome to my channel ");
-// fs.writeFileSync('read.txt'," Thapa technical ,Welcome to my channel ");
-// fs.appendFileSync('read.txt',"Ruchika sharma");
-// const buf_data=fs.readFileSync('read.txt');
-// console.log(buf_data);
-// org_data=buf_data.toString();
-// console.log(org_data);
-fs.renameSync('read.txt',"readwrite.txt");
