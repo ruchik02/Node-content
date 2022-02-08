fa7024de21bba68fb498e64685593c81ec832762
ruchik02 <ruchika02>
2022-02-08 12:27:56 +0530

    http server create a web server

---
+++ Htttpserver/index.js
@@ -0,0 +1,8 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    res.end('hello from other side');
+
+})
+server.listen(8000,"127.0.0.1",()=>{
+     console.log("listening to the port 8000");
+})
