9c46e6931667f43cc0a733c524f18299f4623fd8
ruchik02 <ruchika02>
2022-02-08 13:21:59 +0530

    request and response in httpserver

--- Htttpserver/index.js
+++ Htttpserver/index.js
@@ -1,6 +1,16 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    res.end('hello from other side');
+    // console.log(req.url);
+    if(req.url=='/'){
+        res.end('hello from home page');
+    }
+    else if(req.url=='/about'){
+        res.end('hello from about page');
+    }else{
+        res.writeHead(404,{"Content-type":"text/html"});
+        res.end("<h1>page not found</h1>");
+    }
+ 
 
 })
 server.listen(8000,"127.0.0.1",()=>{
