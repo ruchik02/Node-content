df6b0e166b3705151b0193270c10375c3eee0747
ruchik02 <ruchika02>
2022-02-12 16:18:32 +0530

    create sign in and sign up

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,6 @@ router.get('/sign-up',userController.signUp);
 
 router.post('/create',userController.create);
 
+router.post('/create-session',userController.createSession);
+
 module.exports=router;
